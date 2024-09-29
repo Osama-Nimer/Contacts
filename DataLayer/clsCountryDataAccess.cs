5e37194c150a14c7acebb0a0ab607421f1b06507
Osama-Nimer <osamafares23@yahoo.com>
2024-09-29 16:34:01 +0300

    Data_Find_GetAll

--- DataLayer/clsCountryDataAccess.cs
+++ DataLayer/clsCountryDataAccess.cs
@@ -1,13 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataLayer
 {
-    class clsCountryDataAccess
+    class clsCountriesDataAccess
     {
+        public static bool GetAllCountries(int CountryID, ref String CountryName, ref String Code, String PhoneCode)
+        {
+            bool isFound =false;
+            SqlConnection conn = new SqlConnection(ConnString.ConnectionString);
+            String Query = "select CountryID from Countries where CountryID = @ID";
+            SqlCommand cmd = new SqlCommand(Query, conn);
+            cmd.Parameters.AddWithValue("ID", CountryID);
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    isFound = true;
+                    CountryName = (String)reader["CountryName"];
+                    Code = Convert.ToString(reader["Code"]);
+                    PhoneCode = Convert.ToString(reader["PhoneCode"]);
+                    reader.Close();
+                }
+                else
+                    isFound = false;
+            }
+            catch (Exception)
+            {
 
+                throw;
+            }
+            finally { conn.Close(); }
+            return isFound;
+        }
+
+        public static bool IsExist(int ID)
+        {
+            bool IsExist=false;
+            SqlConnection conn = new SqlConnection(ConnString.ConnectionString);
+            String Query = "select CountryID from Countries where CountryID = @ID";
+            SqlCommand cmd = new SqlCommand(Query ,conn);
+            cmd.Parameters.AddWithValue("ID", ID);
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                    IsExist = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return IsExist;
+
+        }
     }
 }
--- bisnesseLayer/clsCountryBusiness.cs
+++ bisnesseLayer/clsCountryBusiness.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace bisnesseLayer
 {
-    internal class clsCountryBusiness
+    internal class clsCountriesBusiness
     {
+        enum enMode
+        {
+            AddNew = 0,
+            Update = 1
+        }
+        enMode Mode = enMode.AddNew;
+        public int CountryID{ get; set; }
+        public String CountryName{ get; set; }
+        public String Code{ get; set; }
+        public String PhoneCode{ get; set; }
+
+        public clsCountriesBusiness()
+        {
+            this.CountryID = -1;
+            this.CountryName = "";
+            this.Code = "";
+            this.PhoneCode = "";
+
+            Mode = enMode.AddNew;
+        }
+
+        public clsCountriesBusiness(int CountryID ,String CountryName ,String Code , String PhoneCode)
+        {
+            this.CountryID = CountryID;
+            this.CountryName = CountryName;
+            this.Code = Code;
+            this.PhoneCode = PhoneCode;
+
+            Mode = enMode.Update;
+        }
+        /*---------------------------------         Country        ---------------------------------------*/
+        public 
+
     }
 }
